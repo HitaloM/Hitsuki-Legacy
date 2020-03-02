00b3723f33c271c4850ba3a0e2982cfd6c4d9ba4
HitaloSama <40531911+HitaloKun@users.noreply.github.com>
2020-03-02 07:18:14 -0300

    Preparing for future organizations

--- hitsuki/__main__.py
+++ hitsuki/__main__.py
@@ -137,9 +137,9 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             buttons = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="📃 HitaloSama's Docs", url="https://telegra.ph/HitaloKun-doc-07-15")]
-                [InlineKeyboardButton(text="🇺🇲 Language", callback_data="main_setlang")]]
-                [InlineKeyboardButton(text="❔ Help", url="https://t.me/LordHitsuki_BOT?start=help"), InlineKeyboardButton(text="🚀 Source", url="http://github.com/HitaloSama/Hitsuki/")]])
+                [[InlineKeyboardButton(text="📃 HitaloSama's Docs", url="https://t.me/EmiliaHikariBot?startgroup=new")],
+                [InlineKeyboardButton(text="⚙️ Connect Group", callback_data="main_connect")],
+                [InlineKeyboardButton(text="🇺🇸 Language", callback_data="main_setlang"), InlineKeyboardButton(text="❓ Help", url="https://t.me/EmiliaHikariBot?start=help")]])
             update.effective_message.reply_text(
                 tl(update.effective_message, PM_START_TEXT).format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 disable_web_page_preview=True,
