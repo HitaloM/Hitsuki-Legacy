932ee749a78f83385fe3a1083c8fc59c86f5938a
HitaloSama <40531911+HitaloKun@users.noreply.github.com>
2020-03-05 08:01:22 -0300

    Testing

---
+++ hitsuki/modules/langs/pt.py
@@ -0,0 +1,5 @@
+
+__lang__ = "¸ Português"
+
+PT = {
+# Global default
--- hitsuki/modules/stickers.py
+++ hitsuki/modules/stickers.py
@@ -219,6 +219,7 @@ def makepack_internal(msg, user, png_sticker, emoji, bot, packname, packnum):
 
 
 __help__ = """
+- /stickerid: reply to a sticker to me to tell you its file ID.
 - /getsticker: reply to a sticker to me to upload its raw PNG file.
 - /kang: reply to a sticker to add it to your pack.
 """
