b9658926fe287dc466bc828c2d558386f324ba44
HitaloSama <40531911+HitaloKun@users.noreply.github.com>
2020-03-06 01:56:51 -0300

    Reverting last changes

--- hitsuki/modules/helper_funcs/misc.py
+++ hitsuki/modules/helper_funcs/misc.py
@@ -63,10 +63,10 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
+             EqInlineKeyboardButton("⬅️ Back", url="https://t.me/LordHitsuki_BOT?start"),
              EqInlineKeyboardButton(">>", callback_data="{}_next({})".format(prefix, modulo_page)))]
     else:
-        pairs += [[EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("⬅️ Back", url="https://t.me/LordHitsuki_BOT?start")]]
 
     return pairs
 
