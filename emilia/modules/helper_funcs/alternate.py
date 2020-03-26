12964fbaf23675cae36465da184c8d4058f11466
HitaloSama <40531911+HitaloSama@users.noreply.github.com>
2020-03-26 02:02:30 -0300

    [core] fixes

--- emilia/modules/helper_funcs/alternate.py
+++ emilia/modules/helper_funcs/alternate.py
@@ -50,16 +50,6 @@ def send_message_raw(chat_id, text, *args, **kwargs):
 					return dispatcher.bot.sendMessage(chat_id, text, *args,**kwargs)
 				except error.BadRequest as err:
 					LOGGER.exception("ERROR: {}".format(err))
-
-		elif str(err) == "Have no rights to send a message":
-			try:
-				dispatcher.bot.leaveChat(message.chat.id)
-				dispatcher.bot.sendMessage(-1001180408164, "I am leave chat `{}`\nBecause of: `Muted`".format(message.chat.title))
-			except error.BadRequest as err:
-				if str(err) == "Chat not found":
-					pass
-		else:
-			LOGGER.exception("ERROR: {}".format(err))
 				'''elif str(err) == "Have no rights to send a message":
 									try:
 										dispatcher.bot.leaveChat(message.chat.id)
