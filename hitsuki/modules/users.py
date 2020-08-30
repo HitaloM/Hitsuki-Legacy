14177a03f278feeb4f44bb22a94598008f9e0df6
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-08-29 21:11:33 -0300

    users: added an import that was forgotten

--- hitsuki/modules/users.py
+++ hitsuki/modules/users.py
@@ -16,6 +16,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import re
+from io import BytesIO
 from time import sleep
 from typing import List
 from telegram import TelegramError, Update, Bot, ParseMode
