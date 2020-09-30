AnimeKaizoku/EnterpriseALRobot

added pyrogram client cus why not

tg_bot/__init__.py
@@ -5,6 +5,7 @@ import time
 import spamwatch
 import telegram.ext as tg
 from telethon import TelegramClient
+from pyrogram import Client, errors
 
 StartTime = time.time()
 
@@ -163,6 +164,8 @@ else:
 updater = tg.Updater(TOKEN, workers=WORKERS)
 telethn = TelegramClient("kigyo", API_ID, API_HASH)
 dispatcher = updater.dispatcher
+
+kp = Client("KigyoPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)

HitaloM/Hitsuki-Legacy

hitsuki/__init__.py
@@ -21,6 +21,7 @@ import yaml
 import spamwatch
 
 from telethon import TelegramClient
+from pyrogram import Client, errors
 import telegram.ext as tg
 
 #Enable logging
@@ -124,6 +125,8 @@ updater = tg.Updater(TOKEN, workers=WORKERS)
 dispatcher = updater.dispatcher
 
 tbot = TelegramClient("hitsuki", API_KEY, API_HASH)
+
+pbot = Client("HitsukiPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 SUDO_USERS = list(SUDO_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)