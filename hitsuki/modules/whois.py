21a77d2ec4a44f6539639cbdec390cdb4536472b
Hitalo <hitalo331@outlook.com>
2020-09-30 23:11:27 -0300

    whois: general update                                                                                                 - adapted from [here](https://github.com/pokurt/Butler-Plus-Bot)

--- hitsuki/modules/whois.py
+++ hitsuki/modules/whois.py
@@ -16,36 +16,49 @@
 from datetime import datetime
 
 from pyrogram import filters
-from pyrogram.types import User, Message
+from pyrogram.types import User, Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.raw import functions
 from pyrogram.errors import PeerIdInvalid
 
 from hitsuki import pbot
 
 
+WHOIS = (
+    "**About [{full_name}](tg://user?id={user_id}):**\n\n"
+    "  - UserID: `{user_id}`\n"
+    "  - First Name: `{first_name}`\n"
+    "  - Last Name: `{last_name}`\n"
+    "  - Username: `{username}`\n"
+    "  - Last Online: `{last_online}`\n\n"
+    "Bio:\n{bio}")
+
+WHOIS_PIC = (
+    "**About [{full_name}](tg://user?id={user_id}):**\n"
+    "  - UserID: `{user_id}`\n"
+    "  - First Name: `{first_name}`\n"
+    "  - Last Name: `{last_name}`\n"
+    "  - Username: `{username}`\n"
+    "  - Last Online: `{last_online}`\n\n"
+    "  - Profile Pics: `{profile_pics}`\n"
+    "  - Last Updated: `{profile_pic_update}`\n\n"
+    "Bio:\n{bio}")
+
+
 def ReplyCheck(message: Message):
     reply_id = None
-
+ 
     if message.reply_to_message:
         reply_id = message.reply_to_message.message_id
-
+ 
     elif not message.from_user.is_self:
         reply_id = message.message_id
-
+ 
     return reply_id
 
-infotext = (
-    "**About [{full_name}](tg://user?id={user_id}):**\n"
-    "  - **UserID:** `{user_id}`\n"
-    "  - **First Name:** `{first_name}`\n"
-    "  - **Last Name:** `{last_name}`\n"
-    "  - **Username:** `{username}`\n"
-    "  - **Online Status:** `{last_online}`\n"
-    "  - **Bio:** {bio}")
 
 def LastOnline(user: User):
     if user.is_bot:
-        return "It's a bot!"
+        return ""
     elif user.status == 'recently':
         return "Recently"
     elif user.status == 'within_week':
@@ -62,6 +75,11 @@ def LastOnline(user: User):
 
 def FullName(user: User):
     return user.first_name + " " + user.last_name if user.last_name else user.first_name
+
+
+def ProfilePicUpdate(user_pic):
+    return datetime.fromtimestamp(user_pic[0].date).strftime("%d.%m.%Y, %H:%M:%S")
+
 
 @pbot.on_message(filters.command('whois'))
 async def whois(client, message):
@@ -83,13 +101,33 @@ async def whois(client, message):
         return
     desc = await client.get_chat(get_user)
     desc = desc.description
-    await message.reply_text(
-            infotext.format(
+    user_pic = await client.get_profile_photos(user.id)
+    pic_count = await client.get_profile_photos_count(user.id)
+    if not user.photo:
+        await message.reply(
+            WHOIS.format(
                 full_name=FullName(user),
                 user_id=user.id,
                 first_name=user.first_name,
-                last_name=user.last_name if user.last_name else "`No last name set up`",
-                username=user.username if user.username else "`No username set up`",
+                last_name=user.last_name if user.last_name else "",
+                username=user.username if user.username else "",
                 last_online=LastOnline(user),
                 bio=desc if desc else "`No bio set up.`"),
             disable_web_page_preview=True)
+    else:
+        await client.send_photo(
+            message.chat.id,
+            user_pic[0].file_id,
+            caption=WHOIS_PIC.format(
+                full_name=FullName(user),
+                user_id=user.id,
+                first_name=user.first_name,
+                last_name=user.last_name if user.last_name else "",
+                username=user.username if user.username else "",
+                last_online=LastOnline(user),
+                profile_pics=pic_count,
+                bio=desc if desc else "`No bio set up.`",
+                profile_pic_update=ProfilePicUpdate(user_pic)),
+            reply_to_message_id=ReplyCheck(message),
+            file_ref=user_pic[0].file_ref,
+        )
