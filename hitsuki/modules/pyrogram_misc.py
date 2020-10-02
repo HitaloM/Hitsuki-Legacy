6abcc082fdca4fe226ba473bcf1e7e1392035131
Hitalo <hitalo331@outlook.com>
2020-10-02 16:49:14 -0300

    pyrogram_misc: licensed under MIT

--- hitsuki/modules/pyrogram_misc.py
+++ hitsuki/modules/pyrogram_misc.py
@@ -1,17 +1,21 @@
-#    Hitsuki (A telegram bot project)
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright (C) 2018-2020 Amano Team <contact@amanoteam.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import html
 import regex
@@ -27,7 +31,7 @@ from hitsuki import pbot
 @pbot.on_message(filters.command('dice'))
 async def dice(c: Client, m: Message):
     dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.message_id)
-    await dicen.reply_text(("🎲 The dice stopped at the number {}").format(number=dicen.dice.value), quote=True)
+    await dicen.reply_text("The dice stopped at the number {number}").format(number=dicen.dice.value), quote=True
     
 
 @pbot.on_message(filters.command("id") & filters.private)
@@ -51,7 +55,7 @@ async def ids_private(c: Client, m: Message):
 @pbot.on_message(filters.command("id") & filters.group)
 async def ids(c: Client, m: Message):
     data = m.reply_to_message or m
-    await m.reply_text("<b>Chat info:</b>\n\n"
+    await m.reply_text("<b>Info:</b>\n\n"
                        "<b>Name:</b> <code>{first_name} {last_name}</code>\n"
                        "<b>Username:</b> @{username}\n"
                        "<b>User ID:</b> <code>{user_id}</code>\n"
