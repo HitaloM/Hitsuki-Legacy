6103bc87d51d402af9274116a6669e0efaf7cc8f
Hitalo <hitalo331@outlook.com>
2020-11-14 23:00:07 -0300

    main: change the logging level to WARNING

--- hitsuki/__init__.py
+++ hitsuki/__init__.py
@@ -27,7 +27,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('log.txt'),
               logging.StreamHandler()],
-    level=logging.INFO)
+    level=logging.WARNING)
 
 LOGGER = logging.getLogger(__name__)
 
