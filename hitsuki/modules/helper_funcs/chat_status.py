b059535eab866612f3447ff2e9b40b6729625939
HitaloSama <hitalo331@outlook.com>
2020-11-23 18:27:39 +0000

    requirements: added telegraph

--- hitsuki/modules/helper_funcs/chat_status.py
+++ hitsuki/modules/helper_funcs/chat_status.py
@@ -54,13 +54,13 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         return True
 
     if not member:
-        # try to fetch from cache first.
+        # Try to fetch from cache first
         try:
            return user_id in ADMIN_CACHE[chat.id]
         except KeyError:
-           # keyerror happend means cache is deleted,
-           # so query bot api again and return user status
-           # while saving it in cache for future useage...
+           # KeyError happend means cache is deleted,
+           # so query Bot API again and return user status
+           # while saving it in cache for future usage
            chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
            admin_list = [x.user.id for x in chat_admins]
            ADMIN_CACHE[chat.id] = admin_list
