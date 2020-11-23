677d45abe505f5e421e7ec6414cb56858386086f
HitaloSama <hitalo331@outlook.com>
2020-11-23 18:17:27 +0000

    admins: imrprovements to admins cache system
    - Refactored admins cache system to a better one
    - Added /admincache command to force refresh admins cache

--- hitsuki/modules/helper_funcs/chat_status.py
+++ hitsuki/modules/helper_funcs/chat_status.py
@@ -14,13 +14,15 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from functools import wraps
+from cachetools import TTLCache
 
 from telegram import Chat, ChatMember, Update, Bot
 
 import hitsuki.modules.sql.admin_sql as admin_sql
-from hitsuki.mwt import MWT
-from hitsuki import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
+from hitsuki import dispatcher, DEL_CMDS, SUDO_USERS, WHITELIST_USERS
 from hitsuki.modules.tr_engine.strings import tld
+
+ADMIN_CACHE = TTLCache(maxsize=512, ttl=60*10)
 
 
 def can_delete(chat: Chat, bot_id: int) -> bool:
@@ -43,8 +45,6 @@ def is_user_ban_protected(chat: Chat,
     return member.status in ('administrator', 'creator')
 
 
-# Cache admin status for 10 mins to avoid extra API requests.
-@MWT(timeout=60 * 10)
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if chat.type == 'private' \
             or user_id in SUDO_USERS \
@@ -54,8 +54,20 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         return True
 
     if not member:
-        member = chat.get_member(user_id)
-    return member.status in ('administrator', 'creator')
+        # try to fetch from cache first.
+        try:
+           return user_id in ADMIN_CACHE[chat.id]
+        except KeyError:
+           # keyerror happend means cache is deleted,
+           # so query bot api again and return user status
+           # while saving it in cache for future useage...
+           chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
+           admin_list = [x.user.id for x in chat_admins]
+           ADMIN_CACHE[chat.id] = admin_list
+
+           if user_id in admin_list:
+               return True
+           return False
 
 
 def is_bot_admin(chat: Chat,
