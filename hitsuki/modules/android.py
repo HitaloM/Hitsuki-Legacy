b2215a1d08dd14343ed94c6cc8cb4ef857f5b653
HitaloSama <hitalo331@outlook.com>
2020-11-24 13:19:28 +0000

    android: merged the OrangeFox module into the Android module

--- hitsuki/modules/android.py
+++ hitsuki/modules/android.py
@@ -33,10 +33,16 @@ from hitsuki.modules.tr_engine.strings import tld
 # have spent so much time of their life into making this module better, stable, and well more supports.
 #
 # v2 - Hitalo (@HitaloSama on GitHub) [Pyrogram Adapt]
-# This module was entirely re-written in pyrogram for the Hitsuki bot by me (Hitalo)
+# This module was entirely re-written in pyrogram for the Hitsuki bot
+# Some commands have been ported/adapted from other bots.
+# 
+# Important credits:
+# * The ofox command was originally developed by MrYacha.
+# * The /twrp, /specs, /whatis, /variants, /samcheck and /samget
+# commands were originally developed by KassemSYR.
 #
 # This module was inspired by Android Helper Bot by Vachounet.
-# None of the code is taken from the bot itself, to avoid confusion
+# None of the code is taken from the bot itself, to avoid confusion.
 # Please don't remove these comment, show respect to module contributors.
 
 fw_links = {"SAMMOBILE": "https://www.sammobile.com/samsung/firmware/{}/{}/",
@@ -546,6 +552,77 @@ async def evo(c: Client, update: Update):
             releases += f'[Uninstaller]({data["uninstaller"]["link"]})\n'
 
     await update.reply_text(releases, disable_web_page_preview=True)
+
+
+# OrangeFox: By @MrYacha, powered by OrangeFox API v2
+@pbot.on_message(filters.command(["orangefox", "of", "fox", "ofox"]))
+async def orangefox(c: Client, update: Update):
+
+    chat_id = update.chat.id
+
+    try:
+        codename = update.command[1]
+    except Exception:
+        codename = ''
+
+    if codename == '':
+        reply_text = tld(chat_id, "fox_devices_title")
+
+        devices = _send_request('device/releases/stable')
+        for device in devices:
+            reply_text += f"\n • {device['fullname']} (`{device['codename']}`)"
+
+        reply_text += '\n\n' + tld(chat_id, "fox_get_release")
+        await update.reply_text(reply_text)
+        return
+
+    device = _send_request(f'device/{codename}')
+    if not device:
+        reply_text = tld(chat_id, "fox_device_not_found")
+        await update.reply_text(reply_text)
+        return
+
+    release = _send_request(f'device/{codename}/releases/stable/last')
+    if not release:
+        reply_text = tld(chat_id, "fox_release_not_found")
+        await update.reply_text(reply_text)
+        return
+
+    reply_text = tld(chat_id, "fox_release_title")
+    reply_text += tld(chat_id, "fox_release_device").format(
+        fullname=device['fullname'],
+        codename=device['codename']
+    )
+    reply_text += tld(chat_id,
+                      "fox_release_version").format(release['version'])
+    reply_text += tld(chat_id, "fox_release_date").format(release['date'])
+    reply_text += tld(chat_id, "fox_release_md5").format(release['md5'])
+
+    if device['maintained'] == 3:
+        status = tld(chat_id, "fox_release_maintained_3")
+    else:
+        status = tld(chat_id, "fox_release_maintained_1")
+
+    reply_text += tld(chat_id, "fox_release_maintainer").format(
+        name=device['maintainer']['name'],
+        status=status
+    )
+
+    btn = tld(chat_id, "btn_dl")
+    url = (release['url'])
+    keyboard = [[InlineKeyboardButton(
+        text=btn, url=url)]]
+    await update.reply_text(reply_text, reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True)
+    return
+
+
+def _send_request(endpoint):
+    API_HOST = 'https://api.orangefox.download/v2'
+    response = get(API_HOST + "/" + endpoint)
+    if response.status_code == 404:
+        return False
+
+    return json.loads(response.text)
 
 
 __help__ = True
