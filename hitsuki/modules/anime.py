275d169d4c5aedca62350b9e239355934245cb35
HitaloSama <hitalo331@outlook.com>
2020-11-25 04:58:27 +0000

    anime: merged nhentai into anime module

--- hitsuki/modules/anime.py
+++ hitsuki/modules/anime.py
@@ -19,6 +19,7 @@ import textwrap
 import bs4
 import jikanpy
 import requests
+from telegraph import Telegraph
 from inspect import getfullargspec
 
 from pyrogram import Client, filters
@@ -27,6 +28,8 @@ from pyrogram.types import Message, Update, InlineKeyboardMarkup, InlineKeyboard
 from hitsuki import OWNER_ID, SUDO_USERS, pbot
 from hitsuki.modules.tr_engine.strings import tld
 
+telegraph = Telegraph()
+telegraph.create_account(short_name='hitsuki')
 info_btn = "More Information"
 url = 'https://graphql.anilist.co'
 
@@ -402,6 +405,71 @@ async def upcoming(c: Client, m: Message):
         upcoming_message += f"{entry_num + 1}. {upcoming_list[entry_num]}\n"
 
     await m.reply_text(upcoming_message)
+
+
+@pbot.on_message(filters.command('nhentai'))
+async def nhentai(c: Client, m: Message):
+    query = m.text.split(" ")[1]
+    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(
+        query)
+    await m.reply_text(
+        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Read Here",
+                        url=post_url
+                    )
+                ]
+            ]
+        )
+    )
+
+
+def nhentai_data(noombers):
+    url = f"https://nhentai.net/api/gallery/{noombers}"
+    res = requests.get(url).json()
+    pages = res["images"]["pages"]
+    info = res["tags"]
+    title = res["title"]["english"]
+    links = []
+    tags = ""
+    artist = ''
+    total_pages = res['num_pages']
+    post_content = ""
+
+    extensions = {
+        'j': 'jpg',
+        'p': 'png',
+        'g': 'gif'
+    }
+    for i, x in enumerate(pages):
+        media_id = res["media_id"]
+        temp = x['t']
+        file = f"{i+1}.{extensions[temp]}"
+        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
+        links.append(link)
+
+    for i in info:
+        if i["type"] == "tag":
+            tag = i['name']
+            tag = tag.split(" ")
+            tag = "_".join(tag)
+            tags += f"#{tag} "
+        if i["type"] == "artist":
+            artist = f"{i['name']} "
+
+    for link in links:
+        post_content += f"<img src={link}><br>"
+
+    post = telegraph.create_page(
+        f"{title}",
+        html_content=post_content,
+        author_name="Hitsuki",
+        author_url="https://t.me/LordHitsuki_BOT"
+    )
+    return title, tags, artist, total_pages, post['url'], links[0]
 
 
 async def site_search(client: Client, m: Message, site: str):
