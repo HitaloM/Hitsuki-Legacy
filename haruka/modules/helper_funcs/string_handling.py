52e215f184b407377a69cd26efbfdd13be04c7e7
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-01-06 15:28:44 +0400

    string_handling: Replace emoji with a whitespace instead of nothing
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- haruka/modules/helper_funcs/string_handling.py
+++ haruka/modules/helper_funcs/string_handling.py
@@ -307,4 +307,4 @@ def markdown_to_html(text):
 
 def remove_emoji(inputString):
     """ Remove emojis and other non-safe characters from string """
-    return emoji.get_emoji_regexp().sub(u'', inputString)
+    return re.sub("\s\s+", " ", emoji.get_emoji_regexp().sub(u' ', inputString))
